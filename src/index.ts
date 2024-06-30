8da3dba79893302ccd4d387df09555e9a855fc00
Febri Hardiyanto <73746901+febiens24@users.noreply.github.com>
2024-06-30 08:28:30 +0700

    testing error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { type Application } from 'express'
+import express, { Application } from 'express'
 import 'dotenv/config'
 import appMiddleware from './middleware'
 
@@ -6,7 +6,7 @@ const app: Application = express()
 const port: number =
   process.env.PORT != null ? parseInt(process.env.PORT) : 3000
 
-app.use(appMiddlewarer)
+app.use(appMiddleware)
 
 app.listen(port, () => {
   console.log(`Restapi IGR listerning on http://localhost:${port}`)
